7ad0d31537cc44c7f44bb52eef31a627b41f203f
ayusha priyadarshani <ayusha.antra@gmail.com>
2025-03-18 23:58:08 +1100

    added landing page

--- CEWS/settings.py
+++ CEWS/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Load environment variables from the .env file
-# load_dotenv(dotenv_path=Path(__file__).resolve().parent / '.env')
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
---
+++ myapp/homepageUrl.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from myapp.Views import homePageView 
+
+urlpatterns = [
+    path('', homePageView.home, name="home-page"),
+]
