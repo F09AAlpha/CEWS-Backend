c226f1ba001919de8d444b54e06ea2666df2d880
Aryaman <aryaman.sakthi@gmail.com>
2025-03-20 10:20:50 +1100

    ALPHA-38 Added main models file for easy migrations

---
+++ myapp/models.py
@@ -0,0 +1,5 @@
+# models.py
+# flake8: noqa: F401
+from .Models.economicIndicatorsModel import AnnualEconomicIndicator, MonthlyEconomicIndicator
+from .Models.exchangeRateLatestModel import CurrencyEvent
+from .Models.financialNewsModel import FinancialNews
