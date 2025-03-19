3f3edb1ecdd2cd4c96000da205675e1e6be19577
Aryaman <aryaman.sakthi@gmail.com>
2025-03-20 10:21:57 +1100

    ALPHA-38 Added and migrated models for economic indicators

--- myapp/Models/economicIndicatorsModel.py
+++ myapp/Models/economicIndicatorsModel.py
@@ -1,7 +1,9 @@
 from django.db import models
 
-# Model for storing Annual Economic Indicators like GDP and Inflation
+
 class AnnualEconomicIndicator(models.Model):
+    # Model for storing Annual Economic Indicators like GDP and Inflation
+
     id = models.AutoField(primary_key=True)
     date = models.DateField(unique=True)
     real_gdp = models.DecimalField(max_digits=10, decimal_places=3, null=True, blank=True)
@@ -16,8 +18,10 @@ class AnnualEconomicIndicator(models.Model):
     def __str__(self):
         return f"Annual Indicators ({self.date})"
 
-# Model for storing Monthly Economic Indicators like CPI, Unemployment rate, etc.
+
 class MonthlyEconomicIndicator(models.Model):
+    # Model for storing Monthly Economic Indicators like CPI, Unemployment rate, etc.
+
     id = models.AutoField(primary_key=True)
     date = models.DateField(unique=True)
     cpi = models.DecimalField(max_digits=6, decimal_places=3, null=True, blank=True)
