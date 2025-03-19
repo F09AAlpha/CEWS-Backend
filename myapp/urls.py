8eec1b8d59ccd64af971d7197689741868e2266a
Manojvradan <manojvradanbs@gmail.com>
2025-03-19 21:15:47 +1100

    Linting

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,7 +7,8 @@ from myapp.Views.exchangeRateLatestViews import CurrencyRateView
 urlpatterns = [
     path('fetch-financial-news/', FetchFinancialNewsView.as_view(), name='fetch-financial-news'),
     path('financial-news/', FinancialNewsListView.as_view(), name='financial-news-list'),
-    path('currency/<str:currency_code>/', FetchCurrencyNewsView.as_view(), name='fetch-currency-news') 
+    path('currency/<str:currency_code>/', FetchCurrencyNewsView.as_view(), name='fetch-currency-news'),
+    path('currency-news/', CurrencyNewsListView.as_view(), name='currency-news-list'),
     path('v1/currency/rates/<str:base>/<str:target>/', CurrencyRateView.as_view(), name='currency-rate'),
 
 ]
