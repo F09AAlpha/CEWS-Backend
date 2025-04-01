a88b7500bbad3f898adb3158330c7361a8106a44
ayusha priyadarshani <ayusha.antra@gmail.com>
2025-04-01 21:08:34 +1100

    changed url logic to take functions from the same file for all graphs

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,11 +6,7 @@ from myapp.Views.currencyNewsView import CurrencyNewsListView, FetchCurrencyNews
 from myapp.Views.exchangeRateLatestViews import CurrencyRateView
 from myapp.Views.historicalExchangeRatesView import FetchHistoricalCurrencyExchangeRates
 from myapp.Views.volatilityView import VolatilityAnalysisView
-from myapp.Views.graphView_lastweek import GraphView_lastweek
-from myapp.Views.graphView_lastmonth import GraphView_lastmonth
-from myapp.Views.graphView_last6months import GraphView_last6months
-from myapp.Views.graphView_lastyear import GraphView_lastyear
-from myapp.Views.graphView_last5years import GraphView_last5years
+from myapp.Views.graphView import GraphView_lastweek, GraphView_lastmonth, GraphView_last6months, GraphView_lastyear, GraphView_last5years
 from myapp.Views.registerExchangeRateAlertView import RegisterAlertView
 
 urlpatterns = [
@@ -32,15 +28,15 @@ urlpatterns = [
     ),
     path('v1/economic/indicators/annual/', StoreAnnualIndicatorsView.as_view(), name='store-annual-economic-indicators'),
     path('v1/economic/indicators/monthly/', StoreMonthlyIndicatorsView.as_view(), name='store-monthly-economic-indicators'),
-    path('graph/<str:from_currency>/<str:to_currency>/last-week', GraphView_lastweek.as_view(), name='last-week-graph_view'),
-    path('graph/<str:from_currency>/<str:to_currency>/last-month', GraphView_lastmonth.as_view(), name='last-month-graph_view'),
-    path('graph/<str:from_currency>/<str:to_currency>/last-6-months',
+    path('v1/graph/<str:from_currency>/<str:to_currency>/last-week', GraphView_lastweek.as_view(), name='last-week-graph_view'),
+    path('v1/graph/<str:from_currency>/<str:to_currency>/last-month', GraphView_lastmonth.as_view(), name='last-month-graph_view'),
+    path('v1/graph/<str:from_currency>/<str:to_currency>/last-6-months',
          GraphView_last6months.as_view(),
          name='last-6-months-graph_view'),
-    path('graph/<str:from_currency>/<str:to_currency>/last-year',
+    path('v1/graph/<str:from_currency>/<str:to_currency>/last-year',
          GraphView_lastyear.as_view(),
          name='last-year-graph_view'),
-    path('graph/<str:from_currency>/<str:to_currency>/last-5-years',
+    path('v1/graph/<str:from_currency>/<str:to_currency>/last-5-years',
          GraphView_last5years.as_view(),
          name='last-5-years-graph_view'),
 ]
